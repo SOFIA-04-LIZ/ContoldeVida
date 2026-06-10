7252589d06b54c692964f18217ea30b2283590ec
SOFIA-04-LIZ <sofi.lizeth.js04@gmail.com>
2026-06-10 09:24:47 -0600

    VIDA DEL PRODUCTO EN REPARACION

--- reparacion.js
+++ reparacion.js
@@ -1201,7 +1201,7 @@ function calcularVida(){
 
         resultado = "✔ DENTRO DE FRESCURA";
 
-    }else if(porcentaje > 59  && porcentaje< 80){
+    }else if(porcentaje > 59  && porcentaje< 93){
 
         resultado = "⚠ VALIAR CON ENCARGADO DE FRESCURA";
     } else{
